9ebf7827387ac51db0dd0c0ee8b987763e7dd5f1
gemtunde <gemtunde@gmail.com>
2022-08-22 07:56:07 +0100

    circular chart

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import './featured.scss';
 
 const Featured = () => {
   return (
-    <div className='featured'>Featured</div>
+    <div className='featured'>
+        <div className="top">
+            <h1 className="title">Billers</h1>
+            <p className="description"> billers that joined over the month</p>
+        </div>
+        <div className="bottom">
+            <div className="featuredChart">
+                <CircularProgressbar 
+                value={65} 
+                text={` 65%` } 
+                strokeWidth={10}/>
+            </div>
+        </div>
+        
+    </div>
   )
 }
 
